1acc18bf7430ca8a49ae04f3afb0679b179d0416
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-12 09:25:40 +0300

    Created function to get computer choice.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function randomNumber(max){
   return Math.floor(Math.random() * max + 1);
 }
+
+function getComputerChoice(){
+    let num = randomNumber(3);
+
+    if (num == 1){
+        return "Rock";
+    }
+    else if (num == 2){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
