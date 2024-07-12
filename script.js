096eecc825344e4396e2c67506775f04fe899534
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-12 09:43:54 +0300

    Created function to get human choice.

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function getComputerChoice(){
         return "Scissors";
     }
 }
+
+function getHumanChoice(){
+    let choice;
+
+    for(let i = 0; i < 1;){
+        choice = prompt("Rock, Paper, or Scissors?" ,"ROCK");
+
+        if(choice == "ROCK" || choice == "rock"){
+            return "Rock"
+            i++;
+        }
+        else if(choice == "PAPER" || choice == "paper"){
+            return "Paper"
+            i++;
+        }
+        else if(choice == "SCISSORS" || choice == "scissors"){
+            return "Scissors"
+            i++;
+        }
+        else {
+            alert("Invalid input. Please enter the input with all lower or upper case.");
+        }
+    }
+
+    return choice;
+}
