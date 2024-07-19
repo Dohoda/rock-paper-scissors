10be75a1c84a9d13356c9a84e6630543c6ffef55
Dohoda <173878821+Dohoda@users.noreply.github.com>
2024-07-19 08:56:41 +0300

    Added roundcounts and started working on loop.

--- script.js
+++ script.js
@@ -22,40 +22,58 @@ let computerScore = 0;
 function playRound(humanChoice){
 
     let computerChoice = getComputerChoice();
+    let roundCount = 0;
+
+    const gameRight = document.querySelector(".game-right");
+    const gameLeft = document.querySelector(".game-left");
+
+    const computerResult = document.createElement("p");
+    computerResult.style.color = "red";
+
+    const humanResult = document.createElement("p");
+    humanResult.style.color = "blue";
+
+    const gameResult = document.createElement("p");
 
     if(humanChoice == "Rock" && computerChoice == "Scissors"){
-        console.log("You won! Rock beats Scissors");
+        humanResult.textContent = "You selected Rock!";
+        computerResult.textContent = "Computer selected Scissors!";
+        gameLeft.appendChild(humanResult);
+        gameRight.appendChild(computerResult);
+        roundCount++;
         humanScore++;
     }
     else if(humanChoice == "Rock" && computerChoice == "Paper"){
-        console.log("You lose! Paper beats Rock");
+        roundCount++;
         computerScore++;
     }
     else if(humanChoice == "Paper" && computerChoice == "Scissors"){
-        console.log("You lose! Scissors beats Paper");
+        roundCount++;
         computerScore++;
     }
     else if(humanChoice == "Paper" && computerChoice == "Rock"){
-        console.log("You won! Paper beats Rock");
+        roundCount++;
         humanScore++;
     }
     else if(humanChoice == "Scissors" && computerChoice == "Rock"){
-        console.log("You lose! Rock beats Scissors");
+        roundCount++;
         computerScore++;
     }
     else if(humanChoice == "Scissors" && computerChoice == "Paper"){
-        console.log("You won! Scissors beats Paper");
+        roundCount++;
         humanScore++;
     }
     else{
-        console.log("It was a draw!")
+        
     }
 }
 
 const btnRock = document.querySelector("#rock");
 btnRock.addEventListener("click", function() {playRound("Rock");});
+
 const btnPaper = document.querySelector("#paper");
 btnPaper.addEventListener("click", function() {playRound("Paper");});
+
 const btnScissors = document.querySelector("#scissors");
 btnScissors.addEventListener("click", function() {playRound("Scissors");});
 
